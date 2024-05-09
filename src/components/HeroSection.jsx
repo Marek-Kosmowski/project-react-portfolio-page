264b1f402752060c47e78f22140cabe880f70aa7
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-09 22:35:01 +0200

    Add styluing to button in HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import avatarImage from '../assets/avatar-new-new.png';
 export default function HeroSection() {
   return (
     <section>
-      <div className='grid grid-cols-1 sm:grid-cols-12'>
+      <div className='grid grid-cols-1 sm:grid-cols-12 md:gap-12 '>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className=' text-main-text mb-4 text-4xl sm:text-5xl lg:text-6xl'>
             Hello, I'm Marek
@@ -15,8 +15,14 @@ export default function HeroSection() {
             magni nisi animi ullam?
           </p>
           <div>
-            <button className='px-6 py-3 mr-6 w-full sm:w-fit rounded-full bg-white hover:bg-slate-200 duration-200'>About me</button>
-            <button className='px-6 py-3 w-full sm:w-fit mt-4 rounded-full border border-white text-white bg-transparent hover:bg-[#1b263b] duration-200'>Download CV</button>
+            <button className='px-6 py-3 mr-6 w-full sm:w-fit rounded-full bg-gradient-to-r from-cyan-500 via-sky-600 to-blue-500 '>
+              About me
+            </button>
+            <button className='px-1 py-1 w-full sm:w-fit mt-4 rounded-full text-white bg-gradient-to-r from-cyan-500 via-sky-600 to-blue-500'>
+              <span className='block px-5 py-2 bg-main-color rounded-full hover:bg-[#1b263b] duration-200'>
+                Download CV
+              </span>
+            </button>
           </div>
         </div>
         <div className='  col-span-5 place-self-center mt-8 lg:mt-0'>

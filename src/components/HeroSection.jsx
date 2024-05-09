4f5e04e8fc75e9a11dba82f31a7fd6870bf8b7ae
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-09 21:43:54 +0200

    Add button to HeroSection component and styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,12 +9,15 @@ export default function HeroSection() {
             Hello, I'm Marek
           </h1>
           <p className='text-para-text text-lg mb-6 lg:text-xl'>
-            {' '}
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
             autem porro accusantium suscipit enim sequi id maxime maiores
             temporibus aperiam dolor vitae, perferendis illo. Quibusdam odit
             magni nisi animi ullam?
           </p>
+          <div>
+            <button className='px-6 py-3 mr-6 rounded-full bg-white hover:bg-slate-200 duration-200'>About me</button>
+            <button className='px-6 py-3 rounded-full border border-white text-white bg-transparent hover:bg-[#1b263b] duration-200'>Download CV</button>
+          </div>
         </div>
         <div className='  col-span-5 place-self-center mt-8 lg:mt-0'>
           <div className='rounded-full bg-[#1b263b] w-[450px] h-[450px] relative '>

83ad0f70371d6a12f5ccdaaa80efc3a7c000f4c4
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-19 21:27:50 +0200

    Add Tailwind CSS Animated plugin to the project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ export default function Modal({ children, links }) {
   const dialog = useRef();
   return createPortal(
     <dialog
-      className=' w-full  h-1/4 flex flex-col justify-evenly items-center bg-gray-800 list-none opacity-90'
+      className=' w-full  h-1/4 flex flex-col justify-evenly items-center bg-main-color list-none animate-fade-down animate-duration-[400ms] animate-once'
       ref={dialog}
     >
       {links.map((link, index) => (

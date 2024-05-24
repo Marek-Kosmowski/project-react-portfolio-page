009bda769b7986c7ee844cb52e9cc51ccc99726b
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-24 22:24:16 +0200

    Add styles to ContactForm component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,16 +6,34 @@ export default function ContactForm() {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstname', { required: true })} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='grid grid-cols-2 grid-rows-7 justify-items-center w-1/2 self-center items-center justify-center'
+    >
+      <label htmlFor='subject' className=' justify-self-start text-main-text'>
+        Subject
+      </label>
+      <input
+        {...register('subject', { required: true })}
+        className='col-span-2 w-full py-2 rounded bg-input-bg border-2 border-gray-600'
+      />
+      <label htmlFor='email' className='justify-self-start text-main-text'>
+        Your Email
+      </label>
       <input
         type='text'
-        placeholder='email'
+        placeholder='email@address.com'
         {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+        className='col-span-2 w-full py-2 rounded bg-input-bg border-2 border-gray-600'
       />
-
-      <textarea {...register('Message', { required: true, min: 10 })} />
-      <input type='submit' />
+      <label htmlFor='message' className='justify-self-start text-main-text'>
+        Message
+      </label>
+      <textarea
+        {...register('Message', { required: true, min: 10 })}
+        className='col-span-2 w-full rounded bg-input-bg border-2 border-gray-600'
+      />
+      <input type='submit' className='col-span-2 w-full text-main-text' />
     </form>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         'logo-bg-color': '#001843',
         'header-bg': '#003566',
         'hero-bg': '#90e0ef',
+        'input-bg': '#283645',
       },
       textColor: {
         'main-text': '#e0e1dd',

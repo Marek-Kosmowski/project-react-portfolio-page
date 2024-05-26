c902159570179a363d00d256d109e051813b8846
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-26 22:48:24 +0200

    Fix styling in ContactForm component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -27,16 +27,22 @@ export default function ContactForm() {
     <form
       onSubmit={handleSubmit(sendEmail)}
       ref={form}
-      className='grid grid-cols-2 grid-rows-7 justify-items-center w-1/2 self-center items-center justify-center'
+      className='grid grid-cols-2 grid-rows-7 justify-items-center w-full sm:w-1/2 self-center items-center justify-center'
     >
-      <label htmlFor='subject' className=' justify-self-start text-main-text'>
+      <label
+        htmlFor='subject'
+        className=' justify-self-start self-end text-main-text'
+      >
         Subject
       </label>
       <input
         {...register('subject', { required: true })}
         className='col-span-2 w-full py-2 rounded bg-input-bg border-2 border-gray-600'
       />
-      <label htmlFor='email' className='justify-self-start text-main-text'>
+      <label
+        htmlFor='email'
+        className='justify-self-start text-main-text self-end'
+      >
         Your Email
       </label>
       <input
@@ -45,14 +51,20 @@ export default function ContactForm() {
         {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
         className='col-span-2 w-full py-2 rounded bg-input-bg border-2 border-gray-600'
       />
-      <label htmlFor='message' className='justify-self-start text-main-text'>
+      <label
+        htmlFor='message'
+        className='justify-self-start text-main-text self-end'
+      >
         Message
       </label>
       <textarea
         {...register('message', { required: true, min: 10 })}
         className='col-span-2 w-full rounded bg-input-bg border-2 border-gray-600'
       />
-      <input type='submit' className='col-span-2 w-full text-main-text' />
+      <input
+        type='submit'
+        className='col-span-2 w-full text-main-text rounded-full bg-gradient-to-r from-cyan-500 via-sky-600 to-blue-500 py-2 mt-4 text-base sm:text-lg '
+      />
     </form>
   );
 }

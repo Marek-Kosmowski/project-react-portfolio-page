e1d3453d61e7ddf2c94342aa41114f9fdaf4813d
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-26 22:07:02 +0200

    Fix inputs names, so an email is sent correctly, add reset method to reset inputs values

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,12 +3,13 @@ import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function ContactForm() {
-  const { register, handleSubmit } = useForm();
-  const form = useRef()
+  const { register, handleSubmit, reset } = useForm();
+  const form = useRef();
 
   // const onSubmit = (data) => console.log(data);
 
   const sendEmail = () => {
+    reset();
     emailjs
       .sendForm('contact_service', 'contact_form', form.current, {
         publicKey: '_YlVkgqlZ6o0APVfe',
@@ -42,14 +43,14 @@ export default function ContactForm() {
       <input
         type='text'
         placeholder='email@address.com'
-        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
         className='col-span-2 w-full py-2 rounded bg-input-bg border-2 border-gray-600'
       />
       <label htmlFor='message' className='justify-self-start text-main-text'>
         Message
       </label>
       <textarea
-        {...register('Message', { required: true, min: 10 })}
+        {...register('message', { required: true, min: 10 })}
         className='col-span-2 w-full rounded bg-input-bg border-2 border-gray-600'
       />
       <input type='submit' className='col-span-2 w-full text-main-text' />

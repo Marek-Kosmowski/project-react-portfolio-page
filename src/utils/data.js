21e85e6221b3e818bf18145b11923eb3c280f34d
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-05-27 22:01:58 +0200

    Create data.js file to hold data for about me component

---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+export const DATA = {
+  skills: {
+    title: 'Skills',
+    skills: [
+      'React.js, JavaScript, MySQL, PostgreSQL, HTML, CSS, Linux, Responsive Design, Git ',
+    ],
+  },
+  education: {
+    title: 'Polish-Japanese Academy of Information Technology',
+    degree: 'Bachelor of Computer Science',
+  },
+};
